266d6b7744a800fa376d9daaf26c5e37ea908085
dennie <denitsa.sureva@icloud.com>
2025-08-09 16:32:38 +0200

    feat: functionality for superuser + test

--- app/core/managers.py
+++ app/core/managers.py
@@ -3,6 +3,9 @@ from django.contrib.auth.models import BaseUserManager
 
 class UserManager(BaseUserManager):
     def create_user(self, email, password=None, **kwargs):
+        if not email:
+            raise ValueError('User must have an email address')
+
         user = self.model(
             email=self.normalize_email(email),
             **kwargs
@@ -11,3 +14,13 @@ class UserManager(BaseUserManager):
         user.save(using=self._db)
 
         return user
+    
+    def create_superuser(self, email, password):
+        user = self.create_user(email, password)
+
+        user.is_staff = True
+        user.is_superuser = True 
+
+        user.save(using=self._db)
+
+        return user 
