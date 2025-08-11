05813025e2b32aa07f5fa12eb2e91ca56eef16b3
dennie <denitsa.sureva@icloud.com>
2025-08-11 16:46:18 +0200

    feat: implementation of token api

--- app/user/views.py
+++ app/user/views.py
@@ -1,8 +1,14 @@
 from rest_framework import generics
+from rest_framework.authtoken.views import ObtainAuthToken 
+from rest_framework.settings import api_settings
 
-from user.serializers import UserSerializers
+from user.serializers import UserSerializers, AuthTokenSerializer
 
 
 class CreateUserView(generics.CreateAPIView):
     serializer_class = UserSerializers
+
+class CreateTokenView(ObtainAuthToken):
+    serializer_class = AuthTokenSerializer
+    rendered_classes = api_settings.DEFAULT_RENDERER_CLASSES 
 
