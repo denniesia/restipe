809a3d6fac4799ffcee5a2de4868551b6421dbf1
dennie <denitsa.sureva@icloud.com>
2025-08-13 10:27:35 +0200

    feat: update tags feauture + tests

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -13,19 +13,14 @@ class TagSerializer(serializers.ModelSerializer):
 class RecipeSerializer(serializers.ModelSerializer):
     tags = TagSerializer(many=True, required=False)
 
-
     class Meta:
         model = Recipe
         fields = ['id', 'title', 'time_minutes', 'price', 'link', 'tags']
         read_only_fields = ['id']
 
-    def create(self, validated_data):
-        tags = validated_data.pop('tags', []) #obtains the tags
-        recipe = Recipe.objects.create(
-            **validated_data
-        )
+    def _get_or_create_tags(self, tags, recipe):
         auth_user = self.context['request'].user
-
+   
         for tag in tags:
             tag_obj, created = Tag.objects.get_or_create(
                 user=auth_user,
@@ -33,7 +28,28 @@ class RecipeSerializer(serializers.ModelSerializer):
             )
             recipe.tags.add(tag_obj)
 
+
+    def create(self, validated_data):
+        tags = validated_data.pop('tags', []) #obtains the tags
+        recipe = Recipe.objects.create(
+            **validated_data
+        )
+        self._get_or_create_tags(tags, recipe)
+       
         return recipe
+    
+    def update(self, instance, validated_data):
+        tags = validated_data.pop('tags', None)
+
+        if tags is not None:
+             instance.tags.clear()
+             self._get_or_create_tags(tags, instance)
+
+        for attr, value in validated_data.items():
+            setattr(instance, attr, value)
+
+        instance.save()
+        return instance
     
 
 class RecipeDetailSerializer(RecipeSerializer):
